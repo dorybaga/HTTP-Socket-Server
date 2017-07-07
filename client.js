afc52c2a9c3d0f3799756559363d1b4ae819ec7d
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-06 22:02:30 -1000

    splitting data; finding request line in header

--- client.js
+++ client.js
@@ -8,17 +8,23 @@ const net = require('net');
 const socket = net.createConnection( { host: process.argv[2], port: 80 }, () => {
   console.log('Connected!');
   const host = process.argv[2];
+  const today = new Date('Wed, 08 Jul 2015 22:31:15 GMT');
 
+  // Send HTTP Request
   socket.write(`GET / HTTP/1.1
 Host: ${host}
+Date: ${today}
 Connection: keep-alive
 
 ` );
 
   // Listen, receive data from server, write to terminal
   socket.on('data', (data) => {
-    console.log(data.toString());
-    // process.stdout.write(data);
+    // console.log(data.toString().split('\n'));
+    console.log(data.toString().split('\n')[0]);
+    process.stdout.write(data.toString());
 
+    // When data finished writing to terminal; end connection
+    socket.destroy();
   });
 });
