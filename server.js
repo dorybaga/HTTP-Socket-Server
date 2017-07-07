cd7cccf04dceda0a8c54451e03c2225e5072c45d
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-06 20:00:27 -1000

    server receives request, responds correctly, ends connection. not reading css

--- server.js
+++ server.js
@@ -7,81 +7,43 @@ const fs = require('fs');
 // Create new server
 const server = net.createServer(function(socket){
   socket.on('data', function(data){
-  console.log("connection recieved");
   // console.log(data.toString().split('\n')[0].split(" ")[1]);
 
-  const index = fs.readFileSync('public/index.html', 'utf8', () =>{
+    const index = fs.readFileSync('public/index.html', 'utf8', () =>{
 
-  });
+    });
 
-  const hydrogen = fs.readFileSync('public/hydrogen.html', 'utf8', () =>{
+    const hydrogen = fs.readFileSync('public/hydrogen.html', 'utf8', () =>{
 
-  });
+    });
 
-  const helium = fs.readFileSync('public/helium.html', 'utf8', () =>{
+    const helium = fs.readFileSync('public/helium.html', 'utf8', () =>{
 
-  });
+    });
 
-  const err = fs.readFileSync('public/404.html', 'utf8', () =>{
+    const err = fs.readFileSync('public/404.html', 'utf8', () =>{
 
-  });
+    });
 
-  const css = fs.readFileSync('public/css/styles.css', 'utf8', () =>{
+    const css = fs.readFileSync('public/css/styles.css', 'utf8', () =>{
 
-  });
+    });
+
+    const today = new Date('Wed, 08 Jul 2015 22:31:15 GMT');
 
 
-  const today = new Date('Wed, 08 Jul 2015 22:31:15 GMT \n');
+    function makeHeader(status, type, length, name){
 
-
-  function makeHeader(status, type, name){
-
-    return `HTTP/${status}
+      return `HTTP/${status}
 Server: betaserver
 Date: ${today};
 Content-Type: ${type}; charset=utf-8
-Content-Length: ${name}.length
+Content-Length: ${length}
 Connection: keep-alive
 
 ${name}`;
 
-  }
-
-  console.log(makeHeader( '1.1 200 OK', 'text/html', index ));
-  socket.write(makeHeader( '1.1 200 OK', 'text/html', index ));
-
-
-  var URI = (data.toString().split('\n')[0].split(" ")[1]);
-
-  // switch(URI){
-  //   case "/":
-  //     socket.write(makeHeader( '200 OK', 'text/html', index ));
-  //     break;
-
-  //   case "/index.html":
-  //     socket.write(makeHeader( '200 OK', 'text/html', index ));
-  //     break;
-
-  //   case "/hydrogen.html":
-  //     socket.write(makeHeader( '200 OK', 'text/html', hydrogen ));
-  //     break;
-
-  //   case "/helium.html":
-  //     socket.write(makeHeader( '200 OK', 'text/html', helium ));
-  //     break;
-
-  //   case "/css/styles.css":
-  //     socket.write(makeHeader( '200 OK', 'text/html', css ));
-  //     break;
-
-  //   default:
-  //     socket.write(makeHeader( '404 Not Found', 'text/html', err ));
-  //     break;
-
-  // }
-
-
-
+      }
 
 //   socket.write(`HTTP/1.1 200 OK
 // Server: betaserver
@@ -130,17 +92,51 @@ ${name}`;
 // ${css}`);
 
 
-  socket.destroy();
+  // console.log(makeHeader( '1.1 200 OK', 'text/html', '404', index ));
+  // console.log(index.length, hydrogen.length, helium.length, css.length, err.length);
 
+    var URI = (data.toString().split('\n')[0].split(" ")[1]);
+
+    switch(URI){
+      case "/":
+        socket.write(makeHeader( '1.1 200 OK', 'text/html', '404', index ));
+        socket.destroy();
+        break;
+
+      case "/index.html":
+        socket.write(makeHeader( '1.1 200 OK', 'text/html', '404', index ));
+        socket.destroy();
+        break;
+
+      case "/hydrogen.html":
+        socket.write(makeHeader( '1.1 200 OK', 'text/html', '771', hydrogen ));
+        socket.destroy();
+        break;
+
+      case "/helium.html":
+        socket.write(makeHeader( '1.1 200 OK', 'text/html', '605', helium ));
+        socket.destroy();
+        break;
+
+
+      case "/css/styles.css":
+        socket.write(makeHeader( '1.1 200 OK', 'text/html', '1961', css ));
+        socket.destroy();
+        break;
+
+      default:
+        socket.write(makeHeader( '1.0 404 Not Found', 'text/html', '263', err ));
+        socket.destroy();
+        break;
+
+    }
   });
 
 });
 
 // Make server listen
 server.listen(8080, '0.0.0.0', () => {
-
-  console.log('Server is listening on port 8080');
-
+  console.log('Server is listening on port 8080 \n');
 });
 
 
